d7791eeea97da2c243f26c9047ab80b4d2d85c31
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-03 20:49:13 +0800

    Taking care of missing data step-1 with sklearn libraries

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer as smp
 
 dataset = pd.read_csv('Data.csv')
-# Mendapatkan semua baris dengan ( : ) dan mendapatkan semua kolom kecuali kolom terakhir dengan ( :-1 ) sebagai fitur variabel
+# Mendapatkan semua baris dengan ( : ) dan mendapatkan semua kolom kecuali kolom terakhir dengan ( :-1 ) sebagai
+# fitur variabel
 X = dataset.iloc[:, :-1].values
 # Mendapatkan Kolom Terakhir sebagai vektor variabel
 Y = dataset.iloc[:, -1].values
 
-print(X)
-print(Y)
+# Taking care of missing data
+imputer = smp(missing_values=np.nan, strategy="mean")

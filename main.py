a59a1cbf077ea469fee07c9e395e518d1406f5bc
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-05 01:33:44 +0800

    Taking care of missing data step-2 (change nan value of data to mean with fit_transform)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer as smp
 
+# Import dataset
 dataset = pd.read_csv('Data.csv')
 # Mendapatkan semua baris dengan ( : ) dan mendapatkan semua kolom kecuali kolom terakhir dengan ( :-1 ) sebagai
 # fitur variabel
@@ -11,5 +12,9 @@ X = dataset.iloc[:, :-1].values
 # Mendapatkan Kolom Terakhir sebagai vektor variabel
 Y = dataset.iloc[:, -1].values
 
+# Mengganti dataset yang memiliki angka 0 menjadi nan
+dataset[dataset == 0] = np.nan
+
 # Taking care of missing data
 imputer = smp(missing_values=np.nan, strategy="mean")
+X[:, 1:3] = imputer.fit_transform(X[:, 1:3])

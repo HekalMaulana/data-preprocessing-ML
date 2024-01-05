f964a101339aa61dbdec1cf463b06a3ca97e62ec
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-05 22:12:28 +0800

    Coding Exercise (Encoding categorical data)

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
-# Import Libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.impute import SimpleImputer as smp
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-
-# Import dataset
-dataset = pd.read_csv('Data.csv')
-# Mendapatkan semua baris dengan ( : ) dan mendapatkan semua kolom kecuali kolom terakhir dengan ( :-1 ) sebagai
-# fitur variabel
-X = dataset.iloc[:, :-1].values
-# Mendapatkan Kolom Terakhir sebagai vektor variabel
-Y = dataset.iloc[:, -1].values
-
-# Mengganti dataset yang memiliki angka 0 menjadi nan
-# dataset[dataset == 0] = np.nan
-# print(dataset)
-
-# Taking care of missing data
-imputer = smp(missing_values=np.nan, strategy="mean")
-X[:, 1:3] = imputer.fit_transform(X[:, 1:3])
-
-# Encoding Categorical Data
-
-# Encoding Independent Variable
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder, [0])], remainder="passthrough")
-X = np.array(ct.fit_transform(X))
-
-print(X)
+# # Import Libraries
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# from sklearn.impute import SimpleImputer as smp
+# from sklearn.compose import ColumnTransformer
+# from sklearn.preprocessing import OneHotEncoder
+# from sklearn.preprocessing import LabelEncoder
+#
+# # Import dataset
+# dataset = pd.read_csv('Data.csv')
+# # Mendapatkan semua baris dengan ( : ) dan mendapatkan semua kolom kecuali kolom terakhir dengan ( :-1 ) sebagai
+# # fitur variabel
+# X = dataset.iloc[:, :-1].values
+# # Mendapatkan Kolom Terakhir sebagai vektor variabel
+# Y = dataset.iloc[:, -1].values
+#
+# # Mengganti dataset yang memiliki angka 0 menjadi nan
+# # dataset[dataset == 0] = np.nan
+# # print(dataset)
+#
+# # Taking care of missing data
+# imputer = smp(missing_values=np.nan, strategy="mean")
+# X[:, 1:3] = imputer.fit_transform(X[:, 1:3])
+#
+# # Encoding Categorical Data
+#
+# # Encoding Independent Variable
+# ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder="passthrough")
+# X = np.array(ct.fit_transform(X))
+#
+# print(X)
+#
+# # Encoding Dependent Variable
+# le = LabelEncoder()
+# Y = le.fit_transform(Y)
+#
+# print(Y)
 
 # # Importing the necessary libraries
 # import pandas as pd
@@ -68,3 +75,35 @@ print(X)
 #
 # # Print your updated matrix of features
 # print(X)
+
+# Importing the necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+
+# Load the dataset
+dataset = pd.read_csv("titanic.csv")
+
+# Identify the categorical data
+categorical_features = ['Sex', 'Embarked', 'Pclass']
+
+# Implement an instance of the ColumnTransformer class
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), categorical_features)], remainder="passthrough")
+
+# Apply the fit_transform method on the instance of ColumnTransformer
+X = ct.fit_transform(dataset)
+
+# Convert the output into a NumPy array
+X = np.array(X)
+
+# Use LabelEncoder to encode binary categorical data
+le = LabelEncoder()
+y = le.fit_transform(dataset['Survived'])
+
+
+# Print the updated matrix of features and the dependent variable vector
+print("Updated matrix of features: \n", X)
+print("Updated dependent variable vector : \n", y)
